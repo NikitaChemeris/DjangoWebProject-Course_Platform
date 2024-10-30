6edbfb7337f6c00817ce4f8d0a5b31519fabfafa
Nikita <112660966+NikitaChemeris@users.noreply.github.com>
2024-10-30 20:19:26 +0100

    Added new librarys on project

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('course/<int:course_id>', views.single_course, name='single_course')
+    path('<int:course_id>', views.single_course, name='single_course')
 ]
